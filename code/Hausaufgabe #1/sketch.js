8a1cee79ed183d4910d7f586732e1d5ae0c13915
Feiyang <fly0625@hotmail.com>
2021-11-03 00:52:59 +0100

    Create sketch.js

---
+++ code/Hausaufgabe #1/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('rgba(200,190,190,0.6)');
+  
+  
+  
+  
+  fill('gray');
+  stroke('gray');
+  circle(200,200,350);
+  
+  fill('rgba(200,50,100,0.8)');//桃紅色
+  stroke('rgba(200,50,100,0.8)');
+  square(145,145,110);
+
+  fill('rgba(0,120,100,0.8)');//藍綠色
+  stroke('rgba(0,120,100,0.8)');
+  square(145,35,110);
+
+  fill('rgba(80,0,50,0.7)');//暗紅色
+  stroke('rgba(80,0,50,0.7)');
+  square(145,255,110);
+
+
+}
+
