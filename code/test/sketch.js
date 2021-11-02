14b6c8a612b114db233ae9e83b519e81fd1eb59b
Feiyang <fly0625@hotmail.com>
2021-11-02 23:59:31 +0100

    Update sketch.js

--- code/test/sketch.js
+++ code/test/sketch.js
@@ -1,22 +1,29 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(200);
+  background(0);
+  /*
   stroke("red");
-  strokeWeight(40);
-  fill("green");
-  ellipse(150,120,200,200);
+  strokeWeight(1);
+  */
+  
+  fill('gray');
+  stroke('gray');
+  circle(200,200,350);
+  
+  fill('rgba(200,50,100,1)');//桃紅色
+  stroke('rgba(200,50,100,1)');
+  square(145,145,110);
 
-  stroke("green");
-  fill(255,0,0);
-  strokeWeight(50);
-  ellipse(350,120,100,120);
-  fill(200);
-  stroke("yellow");
-  strokeWeight(40);
-  arc(300,300,70,50,0,Math.PI);
+  fill('rgba(0,100,100,1)');//藍綠色
+  stroke('rgba(0,100,100,1)');
+  square(145,35,110);
+
+  fill('rgba(80,0,0,1)');
+  stroke('rgba(80,0,0,1)');
+  square(145,255,110);
 }
 
 
