e0828a306322eaa75b42f7a78bc1798b5770b742
Feiyang <fly0625@hotmail.com>
2021-11-02 12:04:52 +0100

    Update sketch.js

--- code/test/sketch.js
+++ code/test/sketch.js
@@ -5,18 +5,18 @@ function setup() {
 function draw() {
   background(200);
   stroke("red");
-  strokeWeight(5);
+  strokeWeight(40);
   fill("green");
   ellipse(150,120,200,200);
 
   stroke("green");
   fill(255,0,0);
-  strokeWeight(10);
+  strokeWeight(50);
   ellipse(350,120,100,120);
   fill(200);
   stroke("yellow");
-  strokeWeight(30);
-  arc(300,300,100,100,0,Math.PI);
+  strokeWeight(40);
+  arc(300,300,70,50,0,Math.PI);
 }
 
 
