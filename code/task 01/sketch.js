65ee38ffa3bc6e1ad5b36f3b3872e4f8ae0e0e93
Feiyang <fly0625@hotmail.com>
2021-11-03 10:20:41 +0100

    Create sketch.js

---
+++ code/task 01/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('rgba(200,190,190,0.6)');
+  
+  
+  
+  
+  
+ 
+ stroke('green');
+ fill('rgba(190,0,130,0.3)');
+ arc(0,200,100,400,Math.PI/180*270,Math.PI/180*450,PIE);
+ arc(50,200,200,400,Math.PI/180*270,Math.PI/180*450,CHORD);
+ arc(150,200,300,400,Math.PI/180*270,Math.PI/180*450,CHORD);
+ arc(300,200,400,400,Math.PI/180*270,Math.PI/180*450,CHORD);
+ 
+}
+
+
